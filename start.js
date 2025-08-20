1a7c7c3ad9469c6f5c8a1c8167f5c65fb81d5527
anduarte3 <andreqt8@gmail.com>
2025-08-20 10:58:11 +0100

    bundling all commands files together

--- start.js
+++ start.js
@@ -1,12 +1,36 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-var fs = require('node:fs');
-var path = require('node:path');
-var _a = require('discord.js'), Client = _a.Client, Collection = _a.Collection, Events = _a.Events, GatewayIntentBits = _a.GatewayIntentBits;
-var token = process.env.TOKEN;
-// Create a new client instance
-var client = new Client({ intents: [GatewayIntentBits.Guilds] });
+import 'dotenv/config';
+import './types.ts';
+import * as fs from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const token = process.env.TOKEN;
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        }
+        else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+client.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand())
+        return;
+    console.log(interaction);
+});
 // Log in to Discord with your client's token
+console.log("Logged in!");
 client.login(token);
--- start.ts
+++ start.ts
@@ -1,13 +1,40 @@
 import 'dotenv/config'
-import fs from 'node:fs';
-import path from 'node:path';
+import './types.ts'
+import * as fs from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const token = process.env.TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 client.commands = new Collection();
 
-// Log in to Discord with your client's token
-client.login(token);
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+});
+
+// Log in to Discord with your client's token
+console.log("Logged in!")
+client.login(token);
